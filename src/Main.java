edcfe16a6b829094fea62feb211190a21b40036e
yank5 <yank5@nycstudents.net>
2022-11-28 12:27:17 -0500

    aa

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
+import javax.swing.*;
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scan=new Scanner(System.in);
+        System.err.print("Exception in thread \"main\" java.lang.NullPointerException: Cannot invoke \"java.run\" because the return value of \"java.lang.System.console()\" is null\n" +
+                "\tat Main.main(Main.java:1)");
+
     }
 }
